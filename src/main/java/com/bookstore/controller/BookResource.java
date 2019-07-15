7094cb8cea0f1b62354d6ba2c76c410775b4d01d
NIKHIL Kumar <arekumar@in.ibm.com>
2019-07-15 23:10:57 +0530

    adding other image services

--- src/main/java/com/bookstore/controller/BookResource.java
+++ src/main/java/com/bookstore/controller/BookResource.java
@@ -1,17 +1,27 @@
 package com.bookstore.controller;
 
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.sql.Blob;
+import java.sql.SQLException;
 
+import javax.sql.rowset.serial.SerialBlob;
+import javax.sql.rowset.serial.SerialException;
+import javax.activation.FileTypeMap;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,7 +58,7 @@ public class BookResource {
 		MultipartFile multipartFile = multiHttpServletRequest.getFile(it.next());
 		String fileName = id+".png";
 		byte[] bytes = multipartFile.getBytes();
-		BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream("src/main/resources/static/image/book/book"+fileName));
+		BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream("src/main/resources/static/image/book/"+fileName));
 		stream.write(bytes);
 		stream.close();
 		return Collections.singletonMap("upload is successful","ok working session");
@@ -56,7 +66,7 @@ public class BookResource {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 		
-		return Collections.singletonMap("upload is failed","ok working session");
+		return Collections.singletonMap("upload is failed"," non working");
 	}
 	}
 	
@@ -71,6 +81,23 @@ public class BookResource {
 	public Book getBook(@PathVariable("id") Long id) {
 		return bookResource.findOneBook(id);
 		
+	}
+	
+	/*
+	 * @GetMapping(value = "/image") public @ResponseBody byte[] getImage() throws
+	 * IOException { InputStream in = new ByteArrayInputStream
+	 * .getResourceAsStream("src/main/resources/static/image/book/book37.png");
+	 * 
+	 * return IOUtils.toByteArray(in); }
+	 */
+
+	@GetMapping(value = "/image/{id}")
+	public ResponseEntity<byte[]> getImage(@PathVariable("id") Long id) throws IOException  {
+		String filepath = "src/main/resources/static/image/book/"+ String.valueOf(id)+".png";
+		File img = new File(filepath);
+		return ResponseEntity.ok()
+				.contentType(MediaType.valueOf(FileTypeMap.getDefaultFileTypeMap().getContentType(img)))
+				.body(Files.readAllBytes(img.toPath()));
 	}
 	
 }
